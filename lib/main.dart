4d20bd948a7b282189cd2ef9c95bd1c2fbf28f86
shrijalshr <shrijalshrestha313@gmail.com>
2022-02-05 17:34:41 +0545

    minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:news_app/news_screen/views/news_screen.dart';
+import 'package:news_app/blocs/full_news_blocs/fullnews_bloc.dart';
+import 'package:news_app/views/full_news_screen.dart';
 import 'package:news_app/theme/app_theme.dart';
+import 'package:news_app/views/home_page.dart';
 import 'package:news_app/views/splash_screen.dart';
 
-import 'blocs/news_bloc/bloc/news_bloc.dart';
-import 'category/category_bloc/category_bloc.dart';
+import 'blocs/category_bloc/category_bloc.dart';
+import 'blocs/news_bloc/news_bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,11 +26,16 @@ class MyApp extends StatelessWidget {
         ),
         BlocProvider<CategoryBloc>(
           create: (context) => CategoryBloc(),
+        ),
+        BlocProvider<FullNewsBloc>(
+          create: (context) => FullNewsBloc(),
         )
       ],
       child: MaterialApp(
+        
+        debugShowCheckedModeBanner: false,
         theme: AppTheme.lightTheme(context),
-        home: const FullNewsScreen(),
+        home: HomePage(),
       ),
     );
   }
