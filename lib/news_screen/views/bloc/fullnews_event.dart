df09773c3f11779c5b4d2ba96b9200b08dafeea3
shrijalshr <shrijalshrestha313@gmail.com>
2022-02-05 17:40:19 +0545

    minor changes

--- lib/news_screen/views/bloc/fullnews_event.dart
+++
@@ -1,8 +0,0 @@
-part of 'fullnews_bloc.dart';
-
-abstract class FullnewsEvent extends Equatable {
-  const FullnewsEvent();
-
-  @override
-  List<Object> get props => [];
-}
---
+++ lib/router/app_router.dart
@@ -0,0 +1,26 @@
+// import 'package:flutter/material.dart';
+// import 'package:news_app/news_screen/views/news_screen.dart';
+// import 'package:news_app/views/home_page.dart';
+
+// class AppRouter {
+//   Route onGenerateRoute(RouteSettings settings) {
+
+//     switch (settings.name) {
+//       case '/':
+//         var data = settings.arguments as String;
+//         return _pushScreen(HomePage(data    ));
+
+//       case 'fullNews':
+//         return _pushScreen(FullNewsScreen());
+
+//       default:
+//         return _pushScreen(SizedBox());
+//     }
+//   }
+
+//   _pushScreen(screen) {
+//     MaterialPageRoute(
+//       builder: (_) => screen,
+//     );
+//   }
+// }
