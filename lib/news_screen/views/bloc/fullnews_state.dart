295b86662d16de02faa8a4a580d62d9c6aebf2b2
shrijalshr <shrijalshrestha313@gmail.com>
2022-02-05 13:39:49 +0545

    new states for fullnews

--- lib/news_screen/views/bloc/fullnews_state.dart
+++ lib/news_screen/views/bloc/fullnews_state.dart
@@ -7,6 +7,28 @@ abstract class FullNewsState extends Equatable {
   List<Object> get props => [];
 }
 
+class FullNewsInitialState extends FullNewsState {
+  final int id;
+  const FullNewsInitialState({required this.id});
+
+  @override
+  List<Object> get props => [id];
+}
+
 class FullNewsLoadingState extends FullNewsState {}
 
-class NewsLoadedState extends FullNewsState {}
+class FullNewsLoadedState extends FullNewsState {
+  final NewsModel fullNews;
+  const FullNewsLoadedState({required this.fullNews});
+
+  @override
+  List<Object> get props => [fullNews];
+}
+
+class FullNewsErrorState extends FullNewsState {
+  final String errorMessage;
+  const FullNewsErrorState({required this.errorMessage});
+
+  @override
+  List<Object> get props => [errorMessage];
+}
