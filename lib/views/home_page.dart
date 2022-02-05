5240af5fd6421b6cc46817ee99e6b6a2b441157b
shrijalshr <shrijalshrestha313@gmail.com>
2022-02-05 13:40:01 +0545

    refactored newstile

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -11,6 +11,7 @@ import 'package:news_app/models/news_article_model.dart';
 import 'package:news_app/widgets/dash_container.dart';
 
 import 'package:news_app/utils/my_extensions.dart';
+import 'package:news_app/widgets/news_tile.dart';
 
 class HomePage extends StatelessWidget {
   HomePage({Key? key}) : super(key: key);
@@ -113,66 +114,12 @@ class HomePage extends StatelessWidget {
                   physics: const BouncingScrollPhysics(),
                   itemCount: newsList.length,
                   itemBuilder: (context, index) {
-                    return DashContainer(
-                      child: Row(
-                        children: [
-                          Expanded(
-                            flex: 2,
-                            child: Padding(
-                              padding: const EdgeInsets.all(10.0),
-                              child: ClipRRect(
-                                borderRadius: BorderRadius.circular(20),
-                                child: CachedNetworkImage(
-                                  imageUrl:
-                                      "https://images.pexels.com/photos/5662857/pexels-photo-5662857.png?auto=compress&cs=tinysrgb&dpr=1&w=500",
-                                  fit: BoxFit.cover,
-                                  height: 100,
-                                  width: 100,
-                                ),
-                              ),
-                            ),
-                          ),
-                          Expanded(
-                              flex: 5,
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Container(
-                                    padding: const EdgeInsets.symmetric(
-                                        horizontal: 12, vertical: 4),
-                                    decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(10),
-                                      color: const Color(0xFFf2f3f4),
-                                    ),
-                                    child: Text(
-                                      "Politics",
-                                      style: theme.textTheme.subtitle1,
-                                    ),
-                                  ),
-                                  const SizedBox(
-                                    height: 10,
-                                  ),
-                                  Text(
-                                    newsList[index].title ?? "null",
-                                    style: theme.textTheme.headline6!.copyWith(
-                                        fontSize: 18,
-                                        fontWeight: FontWeight.w600),
-                                    maxLines: 1,
-                                    overflow: TextOverflow.ellipsis,
-                                  ),
-                                  const SizedBox(
-                                    height: 10,
-                                  ),
-                                  Text(
-                                    "By ${newsList[index].username}",
-                                    style: theme.textTheme.subtitle2,
-                                  ),
-                                ],
-                              ))
-                        ],
-                      ),
-                    ).containerPadding().horizontalPadding();
+                    return NewsTile(
+                      title: newsList[index].title,
+                      categoryId: newsList[index].categoryId,
+                      imgUrl: newsList[index].image,
+                      userName: newsList[index].username,
+                    ).containerPadding();
                   },
                 );
               } else if (state is NewsErrorState) {
