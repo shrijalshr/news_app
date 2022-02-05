660ad937eb70dc16d7e760d51637b46d19574024
shrijalshr <shrijalshrestha313@gmail.com>
2022-02-05 17:34:55 +0545

    minor change

--- lib/repositories/category_repository.dart
+++ lib/repositories/category_repository.dart
@@ -9,14 +9,12 @@ class CategoryRepository {
     var response = await Network().getData("/category-list");
 
     var data = jsonDecode(response.body);
-    print(data);
+
     List<CategoryModel> _categoryList = [];
 
     if (response.statusCode == 200) {
       data.forEach((e) {
-        print(e);
         CategoryModel _articleModel = CategoryModel.fromJson(e);
-        print(_articleModel.category);
         _categoryList.add(_articleModel);
       });
 
