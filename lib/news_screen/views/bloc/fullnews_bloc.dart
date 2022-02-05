274471ccd55c4406ec9890ed860e3804b51d0d09
shrijalshr <shrijalshrestha313@gmail.com>
2022-02-05 13:40:52 +0545

    news fetching added to FullNewsBloc

--- lib/news_screen/views/bloc/fullnews_bloc.dart
+++ lib/news_screen/views/bloc/fullnews_bloc.dart
@@ -1,13 +1,22 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:news_app/models/news_model.dart';
+import 'package:news_app/repositories/news_repository.dart';
 
 part 'fullnews_event.dart';
 part 'fullnews_state.dart';
 
-class FullnewsBloc extends Bloc<FullnewsEvent, FullnewsState> {
-  FullnewsBloc() : super(FullnewsInitial()) {
-    on<FullnewsEvent>((event, emit) {
-      // TODO: implement event handler
+class FullnewsBloc extends Bloc<FullnewsEvent, FullNewsState> {
+  NewsRepository _newsRepository = NewsRepository();
+  final int id;
+
+  FullnewsBloc(this.id) : super(FullNewsInitialState(id: id)) {
+    on<FullnewsEvent>((event, emit) async {
+      await _newsRepository
+          .fetchNews(id)
+          .then((value) => emit(FullNewsLoadedState(fullNews: value)))
+          .onError((error, stackTrace) =>
+              emit(FullNewsErrorState(errorMessage: error.toString())));
     });
   }
 }
